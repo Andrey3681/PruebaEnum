9066e0cc898ab98c7536d6f5fd1d71702ff51980
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2023-05-25 00:56:48 -0500

    cree la clase Listas, con una lista que guardara la información y el contructor con su metodo get para poderlo obtener y un metodo de añadir por tener alguno

--- pruebaEnum/src/main/java/com/example/pruebaenum/Listas.java
+++ pruebaEnum/src/main/java/com/example/pruebaenum/Listas.java
@@ -1,15 +1,21 @@
 package com.example.pruebaenum;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Listas {
-    private List<String> stringList;
+    private final List<String> stringList;
 
     public Listas() {
-        this.stringList = stringList;
+        this.stringList = new ArrayList<>();
     }
 
     public List<String> getStringList() {
         return stringList;
+    }
+
+
+    public void añadirString(String string){
+        stringList.add(string);
     }
 }
